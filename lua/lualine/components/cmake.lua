13835dbb0832cdb534b9de70fb01df2d0357a0e4
kristian.mostrom <kristian.mostrom@protonmail.com>
2025-09-15 00:15:49 +0200

    added check if we are currently in a cmake project or not

--- lua/lualine/components/cmake.lua
+++ lua/lualine/components/cmake.lua
@@ -9,16 +9,27 @@ end
 local lualine = require("lualine_require")
 local M = lualine.require("lualine.component"):extend()
 
-local default_options = {
-    cmake_separator = "|"
-}
+local function resolve(opt)
+    if type(opt) == "function" then
+        return opt()
+    else
+        return opt
+    end
+end
+
+local default_options = {}
 
 function M:init(options)
     M.super.init(self, options)
     self.options = vim.tbl_deep_extend("keep", self.options or {}, default_options)
+    default_options.separator = resolve(self.options.cmake_separator) or "|"
 end
 
 function M:update_status()
+    if not cmake.is_cmake_project() then
+        return ""
+    end
+
     local status = ""
     local separator = ""
 
@@ -34,7 +45,7 @@ function M:update_status()
 
     if has_build_target then
         if has_build_type then
-            separator = self.options.cmake_separator
+            separator = default_options.separator
         end
         status = string.format("%s%s%s", status, separator, build_target)
     end
